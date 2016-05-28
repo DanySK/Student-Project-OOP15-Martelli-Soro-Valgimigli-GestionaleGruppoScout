3975c1740f2e499fd93cf6f5764402534654a7ad
Giovanni M. Martelli <giovanni.martelli1@gmail.com>
2016-05-28 15:18:47 +0200

    Aggiunta classe WarningNotice

---
+++ src/view/WarningNotice.java
@@ -0,0 +1,22 @@
+package view;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+/** @author Giovanni M. Martelli
+ */
+
+
+/** 
+ * Semplicissima classe  utilizzata per mostrare a video un messaggio d'errore
+ */
+
+/**@param stringa*/
+public class WarningNotice{
+	
+	String str;
+	WarningNotice(String stringa){
+		this.str = stringa;
+		JOptionPane.showMessageDialog(new JFrame(), str);
+	}
+
+}
