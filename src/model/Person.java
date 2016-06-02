4e1abd960e9ecaf1a752fb88e3cea2a7ace35fe1
riccardosoro95 <riccardosoro95@localhost>
2016-06-02 19:04:25 +0200

    aggiunto serial versionUID

--- src/model/Person.java
+++ src/model/Person.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 import java.time.LocalDate;
 
 public class Person implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String name;
 	private String surname;
 	//private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
