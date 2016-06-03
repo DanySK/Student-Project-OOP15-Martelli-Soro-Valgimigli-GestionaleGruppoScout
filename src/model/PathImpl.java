85eaa50fe4711a7affd961a5b6c75bb32bea4635
riccardo soro <riccardo soro@localhost>
2016-06-03 22:45:00 +0200

    aggiunta classe PathImpl (v1.0)

---
+++ src/model/PathImpl.java
@@ -0,0 +1,89 @@
+package model;
+
+import java.io.Serializable;
+
+import control.myUtil.myOptional;
+
+public class PathImpl implements Serializable,model.Path{
+
+	
+
+
+private static final long serialVersionUID = 1L;
+private String level;
+private Integer liv;
+private myOptional<String> school;
+private myOptional<String> family;
+private myOptional<String> relations;
+private myOptional<String> faith;
+
+public PathImpl (){
+	this.liv=1;
+	this.level="scoperta";
+	this.faith=myOptional.empty();
+			
+	this.school=myOptional.empty();
+	this.family=myOptional.empty();
+	this.relations=myOptional.empty();
+}
+public PathImpl (Integer liv,String level,String faith,String school,String family,String relations){
+	this.liv=liv;
+	this.level=level;
+	this.faith=myOptional.of(faith);
+	this.school=myOptional.of(school);
+	this.family=myOptional.of(family);
+	this.relations=myOptional.of(relations);
+}
+public Integer getLiv(){
+	return this.liv;
+}
+public String getLevel(){
+	return this.level;
+}
+public String getSchool(){
+	return this.school.get();
+}
+public void setSchool (String school){
+	this.school=myOptional.of(school);
+}
+public String getFamily(){
+	return this.family.get();
+}
+public void setFamily (String family){
+	this.family=myOptional.of(family);
+}
+public String getRelations(){
+	return this.relations.get();
+}
+public void setRelations (String relations){
+	this.relations=myOptional.of(relations);
+}
+public String getFaith(){
+	return this.faith.get();
+}
+public void setFaith (String faith){
+	this.faith=myOptional.of(faith);
+}
+public boolean livUp(){/*return false if liv is 3*/
+	if (this.liv==3){
+		return false;
+	}
+	this.liv=this.liv+1;
+	updateLevel();
+	return true;
+}
+public boolean livDown(){/*return false if liv is 3*/
+	if (this.liv==1){
+		return false;
+	}
+	this.liv=this.liv-1;
+	updateLevel();
+	return true;
+}
+
+private void updateLevel(){
+	if (this.liv==1) this.level="scoperta";
+	if (this.liv==2) this.level="competenza";
+	if (this.liv==3) this.level="responsabilità";
+}
+}
