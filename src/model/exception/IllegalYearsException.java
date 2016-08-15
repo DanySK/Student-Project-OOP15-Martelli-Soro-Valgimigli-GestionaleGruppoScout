a3d0abaacaea852f00b7fedf7ae7b443a321a4c5
riccardo soro <riccardo soro@localhost>
2016-08-15 18:13:50 +0200

    aggiunta eccezione di eta per i membri

---
+++ src/model/exception/IllegalYearsException.java
@@ -0,0 +1,14 @@
+package model.exception;
+
+public class IllegalYearsException extends Exception{
+	
+    /**
+ * 
+ */
+private static final long serialVersionUID = 1L;
+	private static final String message = "Errore: gli anni del membro devono essere compresi tra 12 e 17";
+	
+	public String getMessage(){
+		return IllegalYearsException.message;
+	}
+}
