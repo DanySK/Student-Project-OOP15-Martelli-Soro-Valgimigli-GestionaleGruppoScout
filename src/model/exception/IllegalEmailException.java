5f33137f0a7bb4b4fa447f5d6d1692bb9360651b
riccardosoro95 <riccardosoro95@localhost>
2016-08-17 16:37:05 +0200

    eccezione

---
+++ src/model/exception/IllegalEmailException.java
@@ -0,0 +1,13 @@
+package model.exception;
+
+public class IllegalEmailException extends Exception {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+private static final String MESSAGE = "Errore: un email non valida";
+	
+	public String getMessage(){
+		return IllegalEmailException.MESSAGE;
+	}
+}
