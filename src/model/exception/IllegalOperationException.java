83edc77b402783a37d3cd756306f8064f04e14fa
riccardosoro95 <riccardosoro95@localhost>
2016-08-17 21:32:32 +0200

    resi pi? chiari i messaggi delle eccezioni

--- src/model/exception/IllegalOperationException.java
+++ src/model/exception/IllegalOperationException.java
@@ -6,7 +6,7 @@ public class IllegalOperationException  extends Exception{
  * 
  */
 private static final long serialVersionUID = 1L;
-	private static final String MESSAGE = "Errore: operazione";
+	private static final String MESSAGE = "Errore: operazione non possibile";
 	
 	public String getMessage(){
 		return IllegalOperationException.MESSAGE;
--- src/model/exception/ObjectAlreadyContainedException.java
+++ src/model/exception/ObjectAlreadyContainedException.java
@@ -6,7 +6,7 @@ public class ObjectAlreadyContainedException extends Exception{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-		private static final String MESSAGE = "Errore: inserito oggetto già presente";
+		private static final String MESSAGE = "Errore: impossibile inserire un oggetto già presente";
 		
 		public String getMessage(){
 			return ObjectAlreadyContainedException.MESSAGE;
