b8e2ac6454eb210f5b8714121818d8cf7919088a
Lorenzo Valgimigli <lorenzo.valgimigli@studio.unibo.it>
2016-08-18 11:22:38 +0200

    improved minor bugs

--- src/control/Unit.java
+++ src/control/Unit.java
@@ -13,118 +13,148 @@ import model.Squadron;
 import model.exception.ObjectAlreadyContainedException;
 import model.exception.ObjectNotContainedException;
 
-  public interface Unit {
+public interface Unit {
 	/**
 	 * 
 	 * @return
 	 */
-	  String getName();
+	String getName();
+
 	/**
 	 * 
 	 * @return
 	 */
-	
-	  Container getContainers();
+
+	Container getContainers();
+
 	/**
 	 * 
 	 * @param exc
 	 */
-	  void addExcursion(Excursion exc);
+	void addExcursion(Excursion exc);
+
 	/**
 	 * 
 	 * @param m
-	 * @throws ObjectAlreadyContainedException 
+	 * @throws ObjectAlreadyContainedException
 	 */
-	  void addMember(Member m) throws ObjectAlreadyContainedException;
+	void addMember(Member m) throws ObjectAlreadyContainedException;
+
 	/**
 	 * 
 	 * @param sq
-	 * @throws ObjectAlreadyContainedException 
+	 * @throws ObjectAlreadyContainedException
 	 */
-	  void createSq(Squadron sq) throws ObjectAlreadyContainedException;
+	void createSq(Squadron sq) throws ObjectAlreadyContainedException;
+
 	/**
 	 * 
 	 * @param m
-	 * @throws ObjectNotContainedException 
+	 * @throws ObjectNotContainedException
 	 */
-	  void removeMember(Member m) throws ObjectNotContainedException;
+	void removeMember(Member m) throws ObjectNotContainedException;
+
 	/**
 	 * 
 	 * @param sq
-	 * @throws ObjectNotContainedException 
-	 * @throws ObjectAlreadyContainedException 
+	 * @throws ObjectNotContainedException
+	 * @throws ObjectAlreadyContainedException
 	 */
-	  void removeSq(Squadron sq) throws ObjectNotContainedException, ObjectAlreadyContainedException;
+	void removeSq(Squadron sq) throws ObjectNotContainedException, ObjectAlreadyContainedException;
+
 	/**
 	 * 
 	 * @param m
 	 * @param sq
-	 * @throws ObjectAlreadyContainedException 
-	 * @throws ObjectNotContainedException 
-	 * @throws MemberSexException 
+	 * @throws ObjectAlreadyContainedException
+	 * @throws ObjectNotContainedException
+	 * @throws MemberSexException
 	 */
-	  void putMemberInSq(Member m, Squadron sq, Roles rl) throws MemberSexException, ObjectNotContainedException, ObjectAlreadyContainedException;
+	void putMemberInSq(Member m, Squadron sq, Roles rl)
+			throws MemberSexException, ObjectNotContainedException, ObjectAlreadyContainedException;
+
 	/**
 	 * Method to change member from a squadron to other one
+	 * 
 	 * @param m
 	 * @param sqOld
 	 * @param sqNew
-	 * @throws ObjectNotContainedException 
-	 * @throws ObjectAlreadyContainedException 
-	 * @throws MemberSexException 
+	 * @throws ObjectNotContainedException
+	 * @throws ObjectAlreadyContainedException
+	 * @throws MemberSexException
 	 */
-	  void changeMemberFromSq(Member m, Squadron sqNew, Roles rl) throws ObjectNotContainedException, MemberSexException, ObjectAlreadyContainedException;
+	void changeMemberFromSq(Member m, Squadron sqNew, Roles rl)
+			throws ObjectNotContainedException, MemberSexException, ObjectAlreadyContainedException;
+
 	/**
 	 * 
 	 * @param name
 	 */
-	  void setName(String name);
+	void setName(String name);
+
 	/**
 	 * Provides the info of unit
+	 * 
 	 * @return
 	 */
-	  String info();
+	String info();
+
 	/**
 	 * Special method that provide a List of pair with the general info
-	 * @return
-	 * A list of array. In each cell there is a specifically information
-	 * 0. Name
-	 * 1. Number of member
-	 * 2. Number of Squadron
-	 * 3. Number of boys
-	 * 4. Number of girls
+	 * 
+	 * @return A list of array. In each cell there is a specifically information
+	 *         0. Name 1. Number of member 2. Number of Squadron 3. Number of
+	 *         boys 4. Number of girls
 	 */
-	  List<Pair<String, String>> getUnitSpecificInfo();
+	List<Pair<String, String>> getUnitSpecificInfo();
+
 	/**
 	 * 
 	 * @return
 	 */
-	  LocalDate getLimitDateToPay();
+	LocalDate getLimitDateToPay();
+
 	/**
 	 * 
 	 * @return
 	 */
-	  List<Member> getMemberDidntPay();
+	List<Member> getMemberDidntPay();
+
 	/**
 	 * 
 	 * @return
 	 */
-	  Reparto getReparto();
+	Reparto getReparto();
+
 	/**
 	 * 
 	 * @param name
 	 */
-	  void removeExcursion(String name);
+	void removeExcursion(String name);
+
 	/**
 	 * 
 	 * @param exc
 	 */
-	  void removeExcursion (Excursion exc);
-	  /**
-	   * 
-	   * @param name
-	   * @param surname
-	   * @return
-	   */
-	  Member getMember(final String name, final String surname);
+	void removeExcursion(Excursion exc);
+
+	/**
+	 * 
+	 * @param name
+	 * @param surname
+	 * @return
+	 */
+	Member getMember(final String name, final String surname);
+
+	/**
+	 * 
+	 * @return
+	 */
+	LocalDate getLastMailSend();
+
+	/**
+	 * 
+	 * @param lastMailSend
+	 */
+	void setLastMailSend(LocalDate lastMailSend);
 }
--- src/control/UnitImpl.java
+++ src/control/UnitImpl.java
@@ -23,7 +23,6 @@ import view.general_utility.WarningNotice;
 
 public class UnitImpl implements Unit, Serializable {
 
-	
 	/**
 	 * 
 	 */
@@ -31,12 +30,14 @@ public class UnitImpl implements Unit, Serializable {
 	final private String nameToSave;
 	final private Reparto rep;
 	final private List<Excursion> excursions;
-	
-	public UnitImpl(final Reparto rep){
-		this.nameToSave = rep.getName().replace(' ','_');
+	private LocalDate lastMailSend = LocalDate.now();
+
+	public UnitImpl(final Reparto rep) {
+		this.nameToSave = rep.getName().replace(' ', '_');
 		this.excursions = new ArrayList<>();
 		this.rep = rep;
 	}
+
 	@Override
 	public String getName() {
 		return this.nameToSave;
@@ -44,8 +45,8 @@ public class UnitImpl implements Unit, Serializable {
 
 	@Override
 	public Container getContainers() {
-		final Container container = new ContainerImpl(this.rep.getAllSquadron(),
-				this.rep.getMembriSenzaSquadriglia(),this.excursions);
+		final Container container = new ContainerImpl(this.rep.getAllSquadron(), this.rep.getMembriSenzaSquadriglia(),
+				this.excursions);
 		return container;
 	}
 
@@ -55,92 +56,92 @@ public class UnitImpl implements Unit, Serializable {
 	}
 
 	@Override
-	public String info(){
+	public String info() {
 		String info = "";
-		info += "Nome reparto: "+ this.rep.getName() + "\n";
-		info += "Capo maschio: "+ this.rep.getCapoM().getName() + "\n";
-		info += "Capo femmina: "+ this.rep.getCapoF().getName() + "\n";
+		info += "Nome reparto: " + this.rep.getName() + "\n";
+		info += "Capo maschio: " + this.rep.getCapoM().getName() + "\n";
+		info += "Capo femmina: " + this.rep.getCapoF().getName() + "\n";
 		info += "Number of members: " + this.getContainers().getMembers().size() + "\n";
 		info += "Number of squadrons: " + this.getContainers().getSquadrons().size() + "\n";
 		return info;
 	}
-	
+
 	@Override
 	public List<Pair<String, String>> getUnitSpecificInfo() {
 		final List<Pair<String, String>> info = new ArrayList<>();
-		
+
 		info.add(new Pair<>("Name", this.getName()));
 		info.add(new Pair<>("Number of member", Integer.toString(this.getContainers().getMembers().size())));
 		info.add(new Pair<>("Number of squadron", Integer.toString(this.getContainers().getSquadrons().size())));
-		info.add(new Pair<>("Number of boys", Integer.toString(this.getContainers().members(e -> e.getSex())
-																				   .size())));
-		info.add(new Pair<>("Number of girls", Integer.toString(this.getContainers().members(e -> ! e.getSex())
-																				   .size())));
+		info.add(new Pair<>("Number of boys", Integer.toString(this.getContainers().members(e -> e.getSex()).size())));
+		info.add(
+				new Pair<>("Number of girls", Integer.toString(this.getContainers().members(e -> !e.getSex()).size())));
 		return info;
 	}
-	
+
 	@Override
 	public void addMember(final Member m) throws ObjectAlreadyContainedException {
-			this.rep.addMembroSenzaSquadriglia(m);
+		this.rep.addMembroSenzaSquadriglia(m);
 	}
-	
+
 	@Override
 	public void createSq(final Squadron sq) throws ObjectAlreadyContainedException {
-			this.rep.addSquadron(sq);
+		this.rep.addSquadron(sq);
 	}
-	
+
 	@Override
 	public void removeMember(final Member m) throws ObjectNotContainedException {
-		if(this.rep.getMembriSenzaSquadriglia().contains(m)){
+		if (this.rep.getMembriSenzaSquadriglia().contains(m)) {
 			this.rep.removeMembroSenzaSquadriglia(m);
-		}else{
-			if(this.rep.getAllMember().contains(m)){
-				for(final Squadron sq : this.rep.getAllSquadron()){
-					if(sq.getMembri().containsKey(m)){
+		} else {
+			if (this.rep.getAllMember().contains(m)) {
+				for (final Squadron sq : this.rep.getAllSquadron()) {
+					if (sq.getMembri().containsKey(m)) {
 						sq.removeMembro(m);
 					}
 				}
 			}
 		}
 	}
-	
+
 	@Override
 	public void removeSq(final Squadron sq) throws ObjectNotContainedException, ObjectAlreadyContainedException {
-			
-		this.rep.removeSquadron(sq);
-		for(final Member member : sq.getMembri().keySet()){
-			this.rep.addMembroSenzaSquadriglia(member);
-		}	
-	}
-	
-	@Override
-	public void putMemberInSq(final Member m,final  Squadron sq,final  Roles rl) throws MemberSexException, ObjectNotContainedException, ObjectAlreadyContainedException {
-			this.rep.spostaMembroInSquadriglia(m, rl, sq);
-	}
-	
-	@Override
-	public void changeMemberFromSq(final Member m,final  Squadron sqNew,final  Roles rl) throws ObjectNotContainedException,
-	MemberSexException, ObjectAlreadyContainedException {
 
-		if( this.rep.getMembriSenzaSquadriglia().contains(m) ){
+		this.rep.removeSquadron(sq);
+		for (final Member member : sq.getMembri().keySet()) {
+			this.rep.addMembroSenzaSquadriglia(member);
+		}
+	}
+
+	@Override
+	public void putMemberInSq(final Member m, final Squadron sq, final Roles rl)
+			throws MemberSexException, ObjectNotContainedException, ObjectAlreadyContainedException {
+		this.rep.spostaMembroInSquadriglia(m, rl, sq);
+	}
+
+	@Override
+	public void changeMemberFromSq(final Member m, final Squadron sqNew, final Roles rl)
+			throws ObjectNotContainedException, MemberSexException, ObjectAlreadyContainedException {
+
+		if (this.rep.getMembriSenzaSquadriglia().contains(m)) {
 			new WarningNotice("Il ragazzo non appartiene a nessuna squadriglia \n verrà comunque assegnato");
-		}else{
+		} else {
 			this.rep.removeMemberFromSquadron(m);
 		}
-		
-		this.putMemberInSq(m, sqNew, rl );
+
+		this.putMemberInSq(m, sqNew, rl);
 	}
-	
+
 	@Override
 	public LocalDate getLimitDateToPay() {
 		return this.rep.getDateToPay();
 	}
-	
+
 	@Override
 	public List<Member> getMemberDidntPay() {
 		return this.rep.getMembersNotPaid(Year.now().getValue());
 	}
-	
+
 	@Override
 	public void addExcursion(final Excursion exc) {
 		this.excursions.add(exc);
@@ -150,40 +151,47 @@ public class UnitImpl implements Unit, Serializable {
 			new WarningNotice("Invio automatico delle email fallito. Controllare la connessione");
 		}
 	}
-	
+
 	@Override
 	public Reparto getReparto() {
 		return this.rep;
 	}
-	
+
 	@Override
 	public void removeExcursion(final String name) {
 		final List<Excursion> exc = this.excursions.stream().filter(e -> e.getName().equalsIgnoreCase(name))
-													  .collect(Collectors.toList());
-		if(Integer.valueOf(exc.size()).equals(0)){
+				.collect(Collectors.toList());
+		if (Integer.valueOf(exc.size()).equals(0)) {
 			new WarningNotice("Nessuna escursione corrisponde al nome: " + name);
 			return;
 		}
 		this.excursions.removeAll(exc);
 	}
-	
+
 	@Override
 	public void removeExcursion(final Excursion exc) {
-		if(this.excursions.contains(exc)){
+		if (this.excursions.contains(exc)) {
 			this.excursions.remove(exc);
-		}else{
+		} else {
 			new WarningNotice("Nessuna escursione corrisponde al nome: " + exc.getName());
 		}
-		
+
 	}
-	
-	public Member getMember(final String name, final String surname){
-		return this.rep.getAllMember()
-				.stream()
-				.filter(e -> e.getName().equalsIgnoreCase(name) 
-						&& e.getSurname().equalsIgnoreCase(surname)).findFirst().get();
+
+	@Override
+	public Member getMember(final String name, final String surname) {
+		return this.rep.getAllMember().stream()
+				.filter(e -> e.getName().equalsIgnoreCase(name) && e.getSurname().equalsIgnoreCase(surname)).findFirst()
+				.get();
 	}
-	
+
+	@Override
+	public LocalDate getLastMailSend() {
+		return this.lastMailSend;
+	}
+	@Override
+	public void setLastMailSend(final LocalDate lastMailSend) {
+		this.lastMailSend = lastMailSend;
+	}
 
 }
-
